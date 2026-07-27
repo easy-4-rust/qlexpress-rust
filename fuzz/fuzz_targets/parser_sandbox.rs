d925e91f9f6f71176749bbeb82224988429b1659
wandl-6A72h <hiwepy@gmail.com>
2026-07-27 13:52:43 +0800

    fix(ci): update fuzz crate import name

--- fuzz/fuzz_targets/parser_sandbox.rs
+++ fuzz/fuzz_targets/parser_sandbox.rs
@@ -1,10 +1,10 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use qlexpress_rust::init_options::InitOptions;
-use qlexpress_rust::ql_options::QLOptions;
-use qlexpress_rust::security::ql_security_strategy::QLSecurityStrategy;
-use qlexpress_rust::Express4Runner;
+use qlexpress::init_options::InitOptions;
+use qlexpress::ql_options::QLOptions;
+use qlexpress::security::ql_security_strategy::QLSecurityStrategy;
+use qlexpress::Express4Runner;
 
 fuzz_target!(|data: &[u8]| {
     if data.len() > 16 * 1024 {
